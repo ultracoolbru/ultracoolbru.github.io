93c471f3dae934135e2cefae85ebb4e436112655
Edward Whitehead <edward@ultracoolbru.com>
2023-07-03 16:34:22 +0400

    -

--- src/components/layout/sections/Navbar.js
+++ src/components/layout/sections/Navbar.js
@@ -1,15 +1,17 @@
 function Navbar() {
     return (
-        <nav>
-            <ul className="navbar">
+        <div className="navbar">
+            <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Resume</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Contact</a></li>
-                <li className="downloadResume fa-li fa fa-check-square"><a href="#">Download Resume</a></li>
             </ul>
-        </nav>
+            <ul>
+                <li className="downloadResume"><a href="#">Download Resume</a></li>
+            </ul>
+        </div>
     )
 }
 
